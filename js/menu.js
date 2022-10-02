c34e89ddf7f558e1ed75af4123af190f685e1d76
manudiiez <manudiiez123@gmail.com>
2022-10-02 11:57:14 -0300

    version 1

--- js/menu.js
+++ js/menu.js
@@ -2,6 +2,7 @@ const menu = document.getElementById('menu')
 const button__open__menu = document.getElementById('button__bars')
 const button__close__menu = document.getElementById('button__xmark')
 const button__bg__menu = document.getElementById('bg__click')
+const links = document.querySelectorAll('.link__button')
 
 button__open__menu.addEventListener('click', () => {
     menu.classList.add('active')
@@ -12,8 +13,30 @@ button__close__menu.addEventListener('click', () => {
 button__bg__menu.addEventListener('click', () => {
     menu.classList.remove('active')
 })
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        menu.classList.remove('active')
 
-
+    })
+})
 window.addEventListener('scroll', function(){
     menu.classList.toggle('bg__dark', window.scrollY>380)
+
+})
+
+const media__container = document.getElementById('media')
+const media__open__button = document.getElementById('fixed__media')
+const media__close__button = document.getElementById('media__close')
+const media__bg = media__container.querySelector('.bg')
+
+media__open__button.addEventListener('click', () => {
+    media__container.classList.add('active')
+})
+media__bg.addEventListener('click', () => {
+    media__container.classList.remove('active')
+    console.log('remove')
+})
+media__close__button.addEventListener('click', () => {
+    media__container.classList.remove('active')
+    console.log('remove')
 })
